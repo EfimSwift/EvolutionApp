cd8369dfba38bfd6c0086102615b4edde42d7059
EfimSwift <a01871893@gmail.com>
2025-03-15 17:17:44 +0200

    functionality added
    logic for transferring photos from the photo selection page to the account page has been added

--- EvolutionApp/EvolutionApp/AccountView/AccountView.swift
+++ EvolutionApp/EvolutionApp/AccountView/AccountView.swift
@@ -8,21 +8,43 @@
 import UIKit
 
 class AccountView: UIViewController {
+    
+    let profileImageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
         title = "Аккаунт"
         
-        let label = UILabel()
-        label.text = "Это экран Аккаунт"
-        label.textColor = .white
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-        
+        setupUI()
+        setupConstraints()
+        loadProfilePhoto()
+    }
+    
+    func setupUI() {
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.backgroundColor = .lightGray
+        profileImageView.layer.cornerRadius = 50
+        profileImageView.clipsToBounds = true
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+    }
+    
+    func setupConstraints() {
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.heightAnchor.constraint(equalToConstant: 100)
         ])
+    }
+    
+    func loadProfilePhoto() {
+        if let imageData = UserDefaults.standard.data(forKey: "UserProfilePhoto"),
+           let loadedImage = UIImage(data: imageData) {
+            profileImageView.image = loadedImage
+        } else {
+            profileImageView.image = UIImage(systemName: "profile.fill")
+        }
     }
 }
